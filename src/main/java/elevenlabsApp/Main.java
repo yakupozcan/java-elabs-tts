024b684cc8229361d8eeb527fba604e0638cbd82
Ekrem <70305889+yakupozcan@users.noreply.github.com>
2025-08-17 10:53:45 +0300

    first commit

---
+++ src/main/java/elevenlabsApp/Main.java
@@ -0,0 +1,33 @@
+
+package elevenlabsApp;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+       
+        anonsButonu.addActionListener(new ActionListener() {
+           
+        });
+
+        
+    }
+}
